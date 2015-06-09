11d238b11a8a44f9eec1ca7ff85d220715857f96
Manuel Contreras <m.contreras.calzon@gmail.com>
2015-06-09 13:41:25 +0200

    Creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -27,6 +27,23 @@ exports.show = function(req, res){
   res.render('quizes/show', { quiz: req.quiz});
 };
 
+// GET /quizes/new
+exports.new = function (req, res){
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta:"Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz})
+};
+
+//GET /quizes/create
+exports.create = function (req, res){
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  })
+};
+
 // GET /quizes/:id/answer
 exports.answer = function(req, res){
   var resultado = 'Incorrecto';
