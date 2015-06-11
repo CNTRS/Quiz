843792c8038fe0597e32f4aaa9d0757320196ea0
Manu <m.conteras.calzon@gmail.com>
2015-06-11 23:42:27 +0200

    Fixed comments render quizid

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -8,7 +8,7 @@ exports.new = function(req, res) {
 // POST /quizes/:quizId/comments
 exports.create = function(req, res) {
   var comment = models.Comment.build(
-      { texto: req.body.comment.texto,          
+      { texto: req.body.comment.texto,
         QuizId: req.params.quizId
         });
 
@@ -18,7 +18,7 @@ exports.create = function(req, res) {
     } else {
        comment // save: guarda en DB campo texto de comment
         .save()
-        .then( function(){ res.redirect('/quizes/'+req.params.quizId)}) 
+        .then( function(){ res.redirect('/quizes/'+req.params.quizId)})
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
   ).catch(function(error){next(error)});
